abee63229d40c8d94c4bd7f330efe313a8267185
lindokuhlemango <mangco678@gmail.com>
2023-07-16 15:52:09 +0200

    format days, display forecast daily, used new apikey

--- src/index.js
+++ src/index.js
@@ -24,46 +24,53 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["sun", "Mon", "Tue", "Wed","Thu","Fri","Sat","Sun"];
+
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sat","Sun","Mon","Tue"];
-
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-
-  forecastHTML = forecastHTML +
-  `     
-   <div class="col-3">
-    <div class="weather-forecast-date">${day}
-      </div>
-
+  let forecastHTML = `<div class="row row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+  forecastHTML = 
+  forecastHTML +
+    `     
+   <div class="col-2">
+    <div class="weather-forecast-date"> ${formatDay(forecastDay.dt)}</div>
+    <img 
+    src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" 
+    width="44px" 
+    alt=""
+    />
     <div class="weather-forecast-temperatures">
+    <span class="weather-forecast-temperature-min"> ${Math.round(
+      forecastDay.temp.min
+    )}° </span>
 
-    <span class="weather-forecast-temperature-min">
-
-    16°/</span>
-
-    <span class="weather-forecast-temperature-max">
-
-    24°</span>
-
-      </div>
-
-    <img src="http://openweathermap.org/img/wn/01d@2x.png" width="40px" alt=""/>
-
-      </div>
-          
-  `;
+    <span class="weather-forecast-temperature-max"> ${Math.round(
+      forecastDay.temp.max
+    )}°</span>
+    </div>
+  </div>    
+`;
+    }
   });
-        forecastHTML = forecastHTML + `</div>`;
-         forecastElement.innerHTML = forecastHTML;
-         console.log(forecastHTML);
-};
+   forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;        
+}
+
+function getForecast(coordinates) {
+  let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function currentTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -84,23 +91,22 @@ function currentTemp(response) {
   cityElement = response.data.name;
   h1.innerHTML = `${cityElement} <img src="https://img.icons8.com/?size=512&id=3723&format=png" width="40px" alt="loctaion"/>`;
 
-  mainicon.setAttribute(
+  mainiconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   mainiconElement.setAttribute("alt", response.data.weather[0].description);
 
-  //getForecast(response.data.coord);
+  getForecast(response.data.coord);
 
 }
 
 function search(city) {
-  let apiKey = "c1cf45e5f52d38632d095b6ef054c012";
+  let apiKey = "b8d0a6b0115d6b7d82d6c9ce2148a3b0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentTemp);
 
 }
-
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -142,5 +148,5 @@ celsiusLink.addEventListener("click" , displayCelsiusTemperature);
 
 
 search("Johannesburg");
-displayForecast();
+//displayForecast();
 
